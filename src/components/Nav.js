765f6b9c29ac2b08d90211358409133e79ebc8cf
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-01-16 17:17:27 +0000

    Minor update to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import HomeIcon from '@material-ui/icons/Home';
