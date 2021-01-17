06d476f79d4d54f6a9da189bf80e1fc589a1259e
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-01-17 18:08:53 +0000

    A fucked EditInfo component. Enjoy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Nav from "./components/Nav";
 import Navigation from "./components/Navigation";
 import Homepage from "./components/Homepage";
 import CreateInfo from "./components/CreateInfo";
-// import EditInfo from "./components/EditInfo";
+import EditInfo from "./components/EditInfo";
 // import CreateUser from "./components/CreateUser";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 
@@ -24,7 +24,7 @@ function App() {
             <Switch>
               <Route path="/" exact component={Homepage} />
               <Route path="/create" component={CreateInfo} />
-              {/* <Route path="/edit/:id" component={EditInfo} /> */}
+              <Route path="/edit/:id" component={EditInfo} />
               {/* <Route path="/user" component={CreateUser} /> */}
             </Switch>
           </Navigation>
--- src/components/EditInfo.js
+++ src/components/EditInfo.js
@@ -1,8 +1,214 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+  } from '@material-ui/pickers';
+
+import {
+    createStyles,
+    withStyles,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    Button,
+    Typography,
+    InputLabel,
+    MenuItem,
+    Select,
+    FormControl,
+    TextField
+
+} from "@material-ui/core";
+
 import './../App.css'
 
 export default class EditInfo extends Component {
-    
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeExperience = this.onChangeExperience.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: '',
+            description: '',
+            experience: 0,
+            date: new Date(),
+
+            users: []
+        };
+    };
+
+    componentDidMount() {
+        // this.setState({
+        //     users: ['test user'],
+        //     username: 'test user'
+        // });
+        axios.get('http://localhost:5000/info/' + this.props.match.params.id)
+            .then(response => {
+                this.setState({
+                    username: response.data.username,
+                    description: response.data.description,
+                    experience: response.data.experience,
+                    date: new Date(response.data.date)
+                })
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username)
+                    });
+                }
+            });
+    };
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    };
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    };
+
+    onChangeExperience(e) {
+        this.setState({
+            experience: e.target.value
+        });
+    };
+
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        });
+    };
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const info = {
+            username: this.state.username,
+            description: this.state.description,
+            experience: this.state.experience,
+            date: this.state.date
+        };
+
+        console.log(info);
+
+        axios.post('http://localhost:5000/info/update/' + this.props.match.params.id, info)
+            .then(res => console.log(res.data));
+
+        window.location = '/';
+    };
+
+    render() {
+        return (
+            <div className="cardRoot">
+                <Typography 
+                    gutterBottom 
+                    variant="h5" 
+                    component="h2" 
+                    style={{ fontFamily: "Raleway", textTransform: "uppercase", letterSpacing: "3px" }}
+                >
+                    Add Information
+                </Typography>
+
+                {/* <CreateInfoCard classes={this.props.classes} /> */}
+
+                <Card>
+                    <CardActionArea>
+                        <CardContent>
+                            <form noValidate autoComplete="off">
+                                <FormControl>
+                                    <InputLabel id="demo-simple-select-label">User</InputLabel>  
+
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={this.state.username}
+                                        onChange={this.onChangeUsername}
+                                        style={{ marginBottom: "35px" }}
+                                    >
+                                        {
+                                            this.state.users.map(function(user) {
+                                                return <option
+                                                            key={user}
+                                                            value={user}
+                                                        >
+                                                            {user}
+                                                        </option>
+                                            })
+                                        };
+                                    </Select>
+                                </FormControl>
+
+                                <TextField 
+                                    fullWidth
+                                    id="standard-basic" 
+                                    label="Description" 
+                                    value={this.state.description}
+                                    onChange={this.onChangeDescription}
+                                    style={{ marginBottom: "35px" }}
+                                />
+
+                                <TextField 
+                                    id="standard-basic" 
+                                    label="Experience (In Years)" 
+                                    value={this.state.experience}
+                                    onChange={this.onChangeExperience}
+                                    style={{ marginBottom: "35px" }}
+                                />
+
+                                <MuiPickersUtilsProvider utils={DateFnsUtils} style={{ padding: '10px', display: 'flex'}}>
+                                    <div style={{ marginLeft: "auto" }}>
+                                        <KeyboardDatePicker
+                                            margin="normal"
+                                            id="date-picker-dialog"
+                                            label="Date picker dialog"
+                                            format="dd/MM/yyyy"
+                                            onChange={this.onChangeDate}
+                                            KeyboardButtonProps={{
+                                                'aria-label': 'change date',
+                                            }}
+                                        />
+                                    </div>
+                                </MuiPickersUtilsProvider>
+                            </form>
+                        </CardContent>
+                    </CardActionArea>
+
+                    <CardActions style={{ padding: '10px', display: 'flex'}}>
+                        <div style={{ marginLeft: "auto" }}>
+                            <Button 
+                                size="large" 
+                                color="primary"
+                                type="submit"
+                                variant="contained"
+                                value="Create Info Log"
+                                onSubmit={this.onSubmit}
+                            >
+                                Submit
+                            </Button>
+                        </div>
+                    </CardActions>
+                </Card>
+            </div>
+        );
+    };
 }
