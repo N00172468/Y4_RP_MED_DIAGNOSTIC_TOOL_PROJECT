0e86d69c9371a999c6d15df6db33b4fb782c3f91
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-02-28 23:15:04 +0000

    Updated edit component

--- src/components/EditInfo.js
+++ src/components/EditInfo.js
@@ -33,21 +33,24 @@ class EditInfo extends Component {
         super(props);
 
         // this.onChangeUsername = this.onChangeUsername.bind(this);
-        this.onChangeUsername = this.onChangeTitle.bind(this);
-        this.onChangeUsername = this.onChangeBody.bind(this);
-        // this.onChangeDescription = this.onChangeDescription.bind(this);
-        // this.onChangeExperience = this.onChangeExperience.bind(this);
-        // this.onChangeDate = this.onChangeDate.bind(this);
+        this.onChangeOverview = this.onChangeOverview.bind(this);
+        this.onChangeSymptoms = this.onChangeSymptoms.bind(this);
+        this.onChangeCauses = this.onChangeCauses.bind(this);
+        this.onChangeRiskFactors = this.onChangeRiskFactors.bind(this);
+        this.onChangeComplications = this.onChangeComplications.bind(this);
+        this.onChangePrevention = this.onChangePrevention.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         this.handleUserSelect = this.handleUserSelect.bind(this)
 
         this.state = {
             title: '',
-            body: '',
-            // username: '',
-            // description: '',
-            // experience: 0,
-            // date: new Date(),
+            // body: '',
+            overview: '',
+            symptoms: '',
+            causes: '',
+            risk_factors: '',
+            complications: '',
+            prevention: '',
 
             users: [],
             loading: true
@@ -61,14 +64,16 @@ class EditInfo extends Component {
         // });
         axios.get('http://localhost:5000/info/' + this.props.match.params.id)
             .then(response => {
-                console.log('respponse!', response)
+                console.log('response!', response)
                 this.setState({
                     title: response.data.title,
-                    body: response.data.body,
-                    // username: response.data.username,
-                    // description: response.data.description,
-                    // experience: response.data.experience,
-                    // date: new Date(response.data.date)
+                    // body: response.data.body,
+                    overview: response.data.overview,
+                    symptoms: response.data.symptoms,
+                    causes: response.data.causes,
+                    risk_factors: response.data.risk_factors,
+                    complications: response.data.complications,
+                    prevention: response.data.prevention
                 })
             })
             .catch(function (error) {
@@ -99,46 +104,54 @@ class EditInfo extends Component {
         });
     };
 
-    onChangeBody(e) {
+    onChangeOverview(e) {
         this.setState({
-            body: e.target.value
+            overview: e.target.value
         });
     };
 
-    // onChangeUsername(e) {
-    //     this.setState({
-    //         username: e.target.value
-    //     });
-    // };
+    onChangeSymptoms(e) {
+        this.setState({
+            symptoms: e.target.value
+        });
+    };
 
-    // onChangeDescription(e) {
-    //     this.setState({
-    //         description: e.target.value
-    //     });
-    // };
+    onChangeCauses(e) {
+        this.setState({
+            causes: e.target.value
+        });
+    };
 
-    // onChangeExperience(e) {
-    //     this.setState({
-    //         experience: e.target.value
-    //     });
-    // };
+    onChangeRiskFactors(e) {
+        this.setState({
+            risk_factors: e.target.value
+        });
+    };
 
-    // onChangeDate(date) {
-    //     this.setState({
-    //         date: date
-    //     });
-    // };
+    onChangeComplications(e) {
+        this.setState({
+            complications: e.target.value
+        });
+    };
+
+    onChangePrevention(e) {
+        this.setState({
+            prevention: e.target.value
+        });
+    };
 
     onSubmit(e) {
         e.preventDefault();
 
         const info = {
             title: this.state.title,
-            body: this.state.body,
-            // username: this.state.username,
-            // description: this.state.description,
-            // experience: this.state.experience,
-            // date: this.state.date
+            // body: this.state.body,
+            overview: this.state.overview,
+            symptoms: this.state.symptoms,
+            causes: this.state.causes,
+            risk_factors: this.state.risk_factors,
+            complications: this.state.complications,
+            prevention: this.state.prevention,
         };
 
         console.log(info);
@@ -205,40 +218,72 @@ class EditInfo extends Component {
                                     onChange={this.onChangeTitle}
                                     style={{ marginBottom: "35px" }}
                                 />
+
+                                <TextField 
+                                    fullWidth
+                                    multiline
+                                    rows={10}
+                                    id="standard-basic" 
+                                    label="Overview" 
+                                    value={this.state.overview}
+                                    onChange={this.onChangeOverview}
+                                    style={{ marginBottom: "35px" }}
+                                />
                                 
                                 <TextField 
                                     fullWidth
                                     multiline
-                                    rows={20}
+                                    rows={10}
                                     id="standard-basic" 
-                                    label="Information" 
-                                    value={this.state.body}
-                                    onChange={this.onChangeBody}
+                                    label="Symptoms" 
+                                    value={this.state.symptoms}
+                                    onChange={this.onChangeSymptoms}
                                     style={{ marginBottom: "35px" }}
                                 />
-
-                                {/* <TextField 
+                                
+                                <TextField 
+                                    fullWidth
+                                    multiline
+                                    rows={10}
                                     id="standard-basic" 
-                                    label="Experience (In Years)" 
-                                    value={this.state.experience}
-                                    onChange={this.onChangeExperience}
+                                    label="Causes" 
+                                    value={this.state.causes}
+                                    onChange={this.onChangeCauses}
                                     style={{ marginBottom: "35px" }}
                                 />
-
-                                <MuiPickersUtilsProvider utils={DateFnsUtils} style={{ padding: '10px', display: 'flex'}}>
-                                    <div style={{ marginLeft: "auto" }}>
-                                        <KeyboardDatePicker
-                                            margin="normal"
-                                            id="date-picker-dialog"
-                                            label="Date picker dialog"
-                                            format="dd/MM/yyyy"
-                                            onChange={this.onChangeDate}
-                                            KeyboardButtonProps={{
-                                                'aria-label': 'change date',
-                                            }}
-                                        />
-                                    </div>
-                                </MuiPickersUtilsProvider> */}
+                                
+                                <TextField 
+                                    fullWidth
+                                    multiline
+                                    rows={10}
+                                    id="standard-basic" 
+                                    label="Risk Factors" 
+                                    value={this.state.risk_factors}
+                                    onChange={this.onChangeRiskFactors}
+                                    style={{ marginBottom: "35px" }}
+                                />
+                                
+                                <TextField 
+                                    fullWidth
+                                    multiline
+                                    rows={10}
+                                    id="standard-basic" 
+                                    label="Complications" 
+                                    value={this.state.complications}
+                                    onChange={this.onChangeComplications}
+                                    style={{ marginBottom: "35px" }}
+                                />
+                                
+                                <TextField 
+                                    fullWidth
+                                    multiline
+                                    rows={10}
+                                    id="standard-basic" 
+                                    label="Prevention" 
+                                    value={this.state.prevention}
+                                    onChange={this.onChangePrevention}
+                                    style={{ marginBottom: "35px" }}
+                                />
                             </form>
                         </CardContent>
                     {/* </CardActionArea> */}
