c01084db50115613f24e358d489b0721981d96be
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-03-28 18:54:29 +0100

    Clean Up

--- src/components/BookmarksPage.js
+++ src/components/BookmarksPage.js
@@ -1,158 +1,166 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 import {
-    Card,
-    CardActionArea,
-    CardActions,
-    CardContent,
-    Button,
-    Typography,
-    createStyles,
-    withStyles
-} from "@material-ui/core"
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+  createStyles,
+  withStyles,
+} from "@material-ui/core";
 
-import './../App.css'
+import "./../App.css";
 
 const styles = (theme) =>
   createStyles({
     root: {
       backgroundColor: theme.palette.secondary.main,
-      margin: theme.spacing(2)
+      margin: theme.spacing(2),
     },
     button: {
-      margin: `0px ${theme.spacing(1)}px`
-
-    }
+      margin: `0px ${theme.spacing(1)}px`,
+    },
   });
-  
-const Info = props => (
-    <Card className={props.classes.root} >
-        <CardActionArea>
-            <CardContent>
-                <Typography
-                    variant="h5" 
-                    color="textSecondary"
-                    component="h5"    
-                >
-                    {props.info.title}
-                </Typography>
 
-                <Typography
-                    variant="body2" 
-                    color="textSecondary"
-                    component="p"    
-                >
-                    {props.info.overview}
-                </Typography>
-            </CardContent>
-        </CardActionArea>
+const Info = (props) => (
+  <Card className={props.classes.root}>
+    <CardActionArea>
+      <CardContent>
+        <Typography variant="h5" color="textSecondary" component="h5">
+          {props.info.title}
+        </Typography>
 
-        <CardActions style={{ padding: '10px', display: 'flex'}}>
-            <div style={{ marginLeft: "auto" }}>
-                <Button 
-                    size="large" 
-                    color="info"
-                    type="submit"
-                    variant="contained"
-                    className={props.classes.button}
-                >
-                    <Link to={"/view/" + props.info._id} style ={{ textDecoration: "none" }}>
-                        View
-                    </Link> 
-                </Button>
-                
-                <Button 
-                    size="large" 
-                    color="primary"
-                    type="submit"
-                    variant="contained"
-                    className={props.classes.button}
-                >
-                    <Link to={"/edit/" + props.info._id} style ={{ textDecoration: "none" }}>
-                        Edit
-                    </Link> 
-                </Button>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.info.overview}
+        </Typography>
+      </CardContent>
+    </CardActionArea>
 
-                <Button 
-                    size="large" 
-                    className={props.classes.button}
-                    color="secondary"
-                    type="submit"
-                    value="Delete"
-                    variant="contained"
-                    onClick={() => { props.deleteInfo(props.info._id) }}
-                    style ={{ textDecoration: "none" }}
-                >
-                    
-                        Delete
-                    
-                </Button>
-            </div>
-        </CardActions>
-    </Card>
+    <CardActions style={{ padding: "10px", display: "flex" }}>
+      <div style={{ marginLeft: "auto" }}>
+        <Button
+          size="large"
+          color="info"
+          type="submit"
+          variant="contained"
+          className={props.classes.button}
+        >
+          <Link
+            to={"/view/" + props.info._id}
+            style={{ textDecoration: "none" }}
+          >
+            View
+          </Link>
+        </Button>
+
+        <Button
+          size="large"
+          color="primary"
+          type="submit"
+          variant="contained"
+          className={props.classes.button}
+        >
+          <Link
+            to={"/edit/" + props.info._id}
+            style={{ textDecoration: "none" }}
+          >
+            Edit
+          </Link>
+        </Button>
+
+        <Button
+          size="large"
+          className={props.classes.button}
+          color="secondary"
+          type="submit"
+          value="Delete"
+          variant="contained"
+          onClick={() => {
+            props.deleteInfo(props.info._id);
+          }}
+          style={{ textDecoration: "none" }}
+        >
+          Delete
+        </Button>
+      </div>
+    </CardActions>
+  </Card>
 );
 
-class Homepage extends Component {
-    constructor(props) {
-        super(props);
+class Bookmark extends Component {
+  constructor(props) {
+    super(props);
 
-        this.deleteInfo = this.deleteInfo.bind(this);
+    this.deleteInfo = this.deleteInfo.bind(this);
 
-        this.state = {
-            info: []
-        };
+    this.state = {
+      info: [],
     };
+  }
 
-    componentDidMount() {
-        axios.get('http://localhost:5000/info/')
-            .then(response => {
-                this.setState({ info: response.data })
-            })
-            .catch((error) => {
-                console.log(error);
-            })
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/info/")
+      .then((response) => {
+        this.setState({ info: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-        axios.get('http://localhost:5000/note/')
-            .then(response => {
-                this.setState({ note: response.data })
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    };
+    axios
+      .get("http://localhost:5000/note/")
+      .then((response) => {
+        this.setState({ note: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    deleteInfo(id) {
-        axios.delete('http://localhost:5000/info/' + id)
-            .then(res => console.log(res.data)
-        );
-        
-        this.setState({
-            info: this.state.info.filter(el => el._id !== id)
-        });
-    };
+  deleteInfo(id) {
+    axios
+      .delete("http://localhost:5000/info/" + id)
+      .then((res) => console.log(res.data));
 
-    render() {
-        return (
-            <div className="cardRoot">
-                <Typography 
-                    gutterBottom 
-                    variant="h5" 
-                    component="h2" 
-                    style={{ fontFamily: "Raleway", textTransform: "uppercase", letterSpacing: "3px" }}
-                >
-                    Bookmarks
-                </Typography>
+    this.setState({
+      info: this.state.info.filter((el) => el._id !== id),
+    });
+  }
 
-                {this.state.info.map(currentInfo => {
-                    return <Info info={currentInfo} deleteInfo={this.deleteInfo} key={currentInfo._id} classes={this.props.classes}/>;
-                    })
-                }
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="cardRoot">
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="h2"
+          style={{
+            fontFamily: "Raleway",
+            textTransform: "uppercase",
+            letterSpacing: "3px",
+          }}
+        >
+          Bookmarks
+        </Typography>
+
+        {this.state.info.map((currentInfo) => {
+          return (
+            <Info
+              info={currentInfo}
+              deleteInfo={this.deleteInfo}
+              key={currentInfo._id}
+              classes={this.props.classes}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-
-export default withStyles(styles)(Homepage)                             
+export default withStyles(styles)(Bookmark);
