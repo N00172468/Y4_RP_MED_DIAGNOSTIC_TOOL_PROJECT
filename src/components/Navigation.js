f0ea8631fa58618cc875171fffadff36494a7eaf
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-03-28 12:03:09 +0100

    Clean Up

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -81,24 +81,11 @@ createStyles({
       width: 'auto',
     },
   },
-
-  searchIcon: {
-    // padding: theme.spacing(0, 2),
-    height: '100%',
-    // position: 'absolute',
-    // pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
   inputRoot: {
     color: 'inherit',
   },
-
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     transition: theme.transitions.create('width'),
     width: '100%',
