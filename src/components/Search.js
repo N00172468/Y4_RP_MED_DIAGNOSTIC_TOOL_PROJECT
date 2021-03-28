126de905bbcb713b3355d873c9ba5cac2b7e9577
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-03-28 13:46:56 +0100

    Commented out extra. Keeping for now just in case

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,14 +7,14 @@ import { Autocomplete, createFilterOptions } from "@material-ui/lab";
 
 const filter = createFilterOptions();
 
-const top100Films = [
-  { title: "The Shawshank Redemption", year: 1994 },
-  { title: "The Godfather", year: 1972 },
-  { title: "The Godfather: Part II", year: 1974 },
-  { title: "The Dark Knight", year: 2008 },
-  { title: "12 Angry Men", year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-];
+// const top100Films = [
+//   { title: "The Shawshank Redemption", year: 1994 },
+//   { title: "The Godfather", year: 1972 },
+//   { title: "The Godfather: Part II", year: 1974 },
+//   { title: "The Dark Knight", year: 2008 },
+//   { title: "12 Angry Men", year: 1957 },
+//   { title: "Schindler's List", year: 1993 },
+// ];
 
 const Search = () => {
   const [value, setValue] = React.useState(null);
