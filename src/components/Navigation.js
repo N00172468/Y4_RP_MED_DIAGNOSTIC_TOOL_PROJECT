44b6a7ea836a0b8992bdf1a10f5833d810f6e982
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-03-28 12:00:38 +0100

    Clean Up

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -69,38 +69,6 @@ createStyles({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
-  // drawer: {
-  //   [theme.breakpoints.up('sm')]: {
-  //     width: drawerWidth,
-  //     flexShrink: 0,
-  //   },
-  // },
-
-  // appBar: {
-  //   [theme.breakpoints.up('sm')]: {
-  //     width: `calc(100% - ${drawerWidth}px)`,
-  //     marginLeft: drawerWidth
-  //   },
-  // },
-
-  // menuButton: {
-  //   marginRight: theme.spacing(2),
-  //   [theme.breakpoints.up('sm')]: {
-  //     display: 'none',
-  //   },
-  // },
-
-  // // necessary for content to be below app bar
-  // toolbar: theme.mixins.toolbar,
-  // drawerPaper: {
-  //   width: drawerWidth,
-  // },
-
-  // content: {
-  //   flexGrow: 1,
-  //   padding: theme.spacing(3),
-  // },
-
   search: {
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
