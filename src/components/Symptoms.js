fdbae501af4457a50c7abf65802421e273bf4b3c
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-03-28 14:08:18 +0100

    Clean Up

--- src/components/Symptoms.js
+++ src/components/Symptoms.js
@@ -1,15 +1,22 @@
-import React, { Component, useState, useEffect } from "react";
+import {
+    React,  
+    Component, 
+    useState, 
+    useEffect 
+} from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-
-import Typography from "@material-ui/core/Typography";
-import { createStyles, withStyles } from "@material-ui/core";
+import {
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    Button,
+    Typography,
+    createStyles,
+    withStyles
+} from "@material-ui/core"
 
 import "./../App.css";
 
